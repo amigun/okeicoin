a2d6bb1581e8bc7c31b395c2e5bfd33a868b9a17
amigun <amigun.tube@gmail.com>
2022-04-24 12:24:23 +0500

    Remove Redis for FSM

--- okeicoin/okeicoin.py
+++ okeicoin/okeicoin.py
@@ -2,8 +2,7 @@ import configparser
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-#from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import handlers
 
@@ -11,8 +10,7 @@ config = configparser.ConfigParser()
 config.read('../config.ini')
 
 bot = Bot(token=config['Telegram']['bot_token'])
-#dp = Dispatcher(bot, storage=MemoryStorage())
-dp = Dispatcher(bot, storage=RedisStorage2('127.0.0.1', 6379, db=5, pool_size=10, prefix='my_fsm_key'))
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 handlers.register_handlers(dp)
 
